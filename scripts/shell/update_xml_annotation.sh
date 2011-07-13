77bf9da8cca88b7cd7633a24be02a54d08213956
Pontus Larsson <pontus@ebi.ac.uk>
2011-07-13 17:08:42 +0000

    Modified to do remapping

--- scripts/shell/update_xml_annotation.sh
+++ scripts/shell/update_xml_annotation.sh
@@ -6,14 +6,26 @@ perldir=${CVSROOTDIR}/code/scripts/
 
 xmlfile=$1
 registryfile=$2
+mapping=$3
 
 filename=`basename ${xmlfile}`
 lrgid=${filename/.xml/}
 outfile=${xmlfile}.new
+target_dir=${SEQDIR}
+tmpdir=${TMPDIR}
 
 echo "Ensembl annotations in ${xmlfile} will be re-generated using the registry configuration in ${registryfile} and perl scripts in ${perldir}..."  
 
-perl ${perldir}/make.lrg.pl -xml_template ${xmlfile} -registry_file ${registryfile} -out ${outfile} -id ${lrgid} -skip_fixed -replace_annotations -use_existing_mapping -skip_host_check -skip_transcript_matching 
+command="perl ${perldir}/make.lrg.pl -xml_template ${xmlfile} -registry_file ${registryfile} -out ${outfile} -id ${lrgid} -skip_fixed -replace_annotations -skip_host_check"
+
+if [ -n "${mapping}" ]
+then
+  command=${command}" -target_dir ${target_dir} -tmpdir ${tmpdir}"
+else
+  command=${command}" -use_existing_mapping"
+fi
+
+$command
 
 echo "An updated LRG XML file for ${lrgid} has been written to ${outfile}"
 
