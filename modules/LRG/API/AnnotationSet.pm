21d45f3fc3ea3004de4d096bd7806815c921a995
Jackie MacArthur <jalm@ebi.ac.uk>
2011-10-31 15:17:31 +0000

    removed conflict

--- modules/LRG/API/AnnotationSet.pm
+++ modules/LRG/API/AnnotationSet.pm
@@ -62,8 +62,10 @@ sub _meta {
   return $meta unless (defined($value));
   
   # Update the meta with the pre-existing and the new value
-  $self->meta([@keep,LRG::API::Meta->new($key,$value)]);
 
+  $self->meta([@keep,LRG::API::Meta->new($key,$value)]);
+  #$self->meta([@keep,$value]);
+  
   return $value;
 }
 
