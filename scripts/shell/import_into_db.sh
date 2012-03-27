0e9ad397456bf362ef9931a2aecc7b0b611e2d07
Laurent Gil <lgil@ebi.ac.uk>
2012-03-27 08:22:43 +0000

    Update the arguments

--- scripts/shell/import_into_db.sh
+++ scripts/shell/import_into_db.sh
@@ -12,10 +12,11 @@ xmlfile=$1
 pass=$2
 
 filename=`basename ${xmlfile}`
-lrgid=${filename/.xml/}
-hgnc=`lrg_gene_name ${xmlfile}`
+lrgid=${filename/.xml.new/}
+#hgnc=`lrg_gene_name ${xmlfile}`
+hgnc=$3
 
 echo "Importing ${lrgid} into db..."
-perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -lrg_id ${lrgid} -purge
+perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -lrg_id ${lrgid} -purge -verbose
 perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -xmlfile ${xmlfile} -lrg_id ${lrgid} -hgnc_symbol ${hgnc}
 
