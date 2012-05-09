9e5a1e2bcfbca9dd833ab25b45c1501d18a97c2b
Laurent Gil <lgil@ebi.ac.uk>
2012-05-09 12:47:58 +0000

    New script to launch pipelines for several LRGs

---
+++ scripts/manage_pipeline.pl
@@ -0,0 +1,64 @@
+#! perl -w
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($data_file, $xml_dir, $new_dir, $tmp_dir, $help);
+GetOptions(
+  'data_file=s'		=> \$data_file,
+  'xml_dir=s'		  => \$xml_dir,
+  'new_dir=s'     => \$new_dir,
+  'tmp_dir=s'     => \$tmp_dir,
+  'help!'         => \$help
+);
+
+usage() if (!defined($data_file) || !defined($xml_dir) || !defined($new_dir) || !defined($tmp_dir) || $help);
+
+my $report_file = "$tmp_dir/pipeline_reports.txt";
+
+`rm -f $report_file`;
+
+open O, ">> $report_file" or die $!;
+print O "Pipeline begins\n\n";
+open F, "< $data_file" or die $!;
+while (<F>) {
+	chomp $_;
+	my ($lrg_id,$hgnc_name,$assembly) = split ("\t", $_);
+	
+	if (!$lrg_id || !$hgnc_name || !$assembly) {
+		print O ($lrg_id) ? "$lrg_id: Can't read the data in the data_file (HGNC name, assembly)\n" : "Can't read the data in the data_file, line $.\n";
+		next;
+	}	
+	print O "$lrg_id: ";
+	`./code/scripts/shell/run_pipeline.sh $lrg_id $hgnc_name $assembly $xml_dir $new_dir $tmp_dir $report_file`;
+
+}
+print O "\nPipeline ends\n";
+close(F);
+close(O);
+
+
+
+sub usage {
+	
+  print qq{
+  Usage: perl manage_pipeline.pl [OPTION]
+  
+  Run pipelines for several LRGs
+	
+  Options:
+    
+      -help           Print this message
+    
+      -data_file      Tabulated file with LRG information to run the pipeline (Required)
+                      Only one LRG per line, with the LRG ID in the first column, the HGNC name in 
+                      the second and the assembly in the third.     
+                      e.g. LRG_5	LEPRE1	GRCh37
+      -report_file    Output file with the information whether a LRG pipeline ran ok or not (Required)
+      -xml_dir        Directory where the LRG file to import are stored (Required)
+      -new_dir        Directory where the results of the pipeline are stored (Required)
+
+  } . "\n";
+  exit(0);
+}
---
+++ scripts/shell/run_full_pipelines.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+. ~/.bashrc
+. ~/.lrgpaths
+
+# e.g. code/scripts/shell/run_full_pipelines.sh LRGs.txt xml_dir new_dir tmp_dir
+# With LRGs.txt like:
+# LRG_1	COL1A1	GRCh37
+# LRG_5	LEPRE1	GRCh37
+# ...
+
+data_file=$1
+xml_dir=$2
+new_dir=$3
+tmp_dir=$4
+
+perldir=${CVSROOTDIR}/code/scripts/
+
+perl ${perldir}/manage_pipeline.pl -data_file ${data_file} -xml_dir ${xml_dir} -new_dir ${new_dir} -tmp_dir ${tmp_dir}
