a934fb991fbdc49bccd4c942e3f8b666a1d0947a
Laurent Gil <lgil@ebi.ac.uk>
2012-06-26 13:31:07 +0000

    Added a new parameter 'error_msg'

--- scripts/shell/import_into_db.sh
+++ scripts/shell/import_into_db.sh
@@ -13,10 +13,10 @@ pass=$2
 
 filename=`basename ${xmlfile}`
 lrgid=${filename/.xml.new/}
-#hgnc=`lrg_gene_name ${xmlfile}`
 hgnc=$3
+error_log=$4
 
 echo "Importing ${lrgid} into db..."
 perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -lrg_id ${lrgid} -purge -verbose
-perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -xmlfile ${xmlfile} -lrg_id ${lrgid} -hgnc_symbol ${hgnc}
+perl ${perldir}/lrg2db.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -xmlfile ${xmlfile} -lrg_id ${lrgid} -hgnc_symbol ${hgnc} -verbose -error_log ${error_log}
 
