da5de67c86e766c9a54088b56644921d92effeb5
Laurent Gil <lgil@ebi.ac.uk>
2013-01-14 09:10:17 +0000

    Script to use in a crontab to generate a dump file of the table related to the LSDB data.

---
+++ scripts/shell/dump_lsdb_tables.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+. ~/.bashrc
+. ~/.lrgpaths
+
+# Relevant paths
+path=$1
+pass=7Ntoz3HH
+
+if [[ ${path} ]] ; then
+  mysqldump -h ${LRGDBHOST} -P ${LRGDBPORT} -u ${LRGDBADMUSER} -p${pass} lrg gene contact lsdb lsdb_contact lsdb_gene lrg_request lrg_status lrg_status_backup > "$1/lrg_dump_"`date +%d`.sql
+else
+  echo "No directory given as argument!"
+fi
+
+
