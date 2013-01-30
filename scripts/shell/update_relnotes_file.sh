e37889d701099f760302376636fe9382c258ae05
Laurent Gil <lgil@ebi.ac.uk>
2013-01-30 09:14:21 +0000

    Script to update the relnotes.txt file and add a new tag to the LRG XML files

---
+++ scripts/shell/update_relnotes_file.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+. ~/.bashrc
+. ~/.lrgpaths
+
+########
+### Update the relnotes.txt file in the CVS repository and the public FTP server
+
+# Relevant paths
+cvspath=${CVSROOTDIR}
+pubpath=${PUBFTP}
+perldir=${CVSROOTDIR}/code/scripts/
+cvsftp=${CVSROOTDIR}/ftp/public/
+tmpdir=''
+tmp=$1
+if [ ${tmp} ]; then
+ tmpdir="-tmp_dir ${tmp}"
+fi
+
+tag_release=`perl ${perldir}/update_relnotes.pl -cvs_dir ${cvspath} -xml_dir ${pubpath} ${tmpdir}`
+
+if [[ ! ${tag_release} || ${tag_release} != release_[0-9]*_[0-9]* ]] ; then 
+  echo "Can't retrieve the LRG release from the script update_relnotes.pl"
+  exit 1
+fi
+
+#### Update and commit CVS ####
+
+
+# 0 - Set the file names & their paths.
+relnotes_fname='relnotes.txt'
+new_relnotes_fname="new_${relnotes_fname}"
+
+record_fname='ftp_record.txt'
+new_record_fname="new_${record_fname}"
+
+if [[ ${tmp} && -d ${tmp} ]]; then
+  new_relnotes="${tmp}/${new_relnotes_fname}"
+  new_record="${tmp}/${new_record_fname}"
+else
+  new_relnotes="${cvsftp}/${new_relnotes_fname}"
+  new_record="${cvsftp}/${new_record_fname}"
+fi
+
+
+# 1 - If OK, copy the new relnotes.txt to the CVS ftp/ and commit it.
+if [[ -e ${new_relnotes} ]] ; then
+  if [[ -s ${new_relnotes} ]] ; then
+    cd ${cvsftp}
+
+		echo "Update relnotes.txt on CVS"
+    cvs update ${relnotes_fname}
+		
+		echo "Copy, commit & tag the new relnotes.txt on CVS"
+    cp ${new_relnotes} "./${relnotes_fname}"
+    cvs ci -m 'New relnote file ${tag_release}' ${relnotes_fname}
+    cvs tag ${tag_release} ${relnotes_fname}
+
+    # 2 - Copy the committed relnotes.txt to the EBI FTP.
+		echo "Copy the new relnotes.txt to the EBI FTP"
+    cp ${relnotes_fname} ${pubpath}
+
+    # 3 - Copy the new ftp_record.txt to the CVS ftp/public/ and commit it.
+		echo "Update ftp_record.txt on CVS"
+    cvs update ${record_fname}
+
+    echo "Copy, commit & tag the new ftp_record.txt on CVS"
+    cp ${new_record}  "./${record_fname}"
+    cvs ci -m 'FTP record of the release ${tag_release}' ${record_fname}
+    cvs tag ${tag_release} ${record_fname}
+
+		# 4 - Tag the LRG XML files
+    cd ${cvspath}/xml
+    echo "Tagging LRG public records"
+    for path in ${pubpath}/LRG_*.xml
+    do
+      filename=`basename ${path}`
+      cvs tag ${tag_release} ${filename}
+    done
+
+    echo "Tagging LRG pending records"
+    for path in ${pubpath}/pending/LRG_*.xml
+    do
+      filename=`basename ${path}`
+      cvs tag ${tag_release} ${filename}
+    done    
+
+  fi
+fi
+
+
+
+
