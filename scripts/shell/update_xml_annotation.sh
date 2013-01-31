24cd280706f9e5078b4f71ab693ae072b5acadab
Laurent Gil <lgil@ebi.ac.uk>
2013-01-31 09:40:18 +0000

    Print the "echo" text in the standard error output

--- scripts/shell/update_xml_annotation.sh
+++ scripts/shell/update_xml_annotation.sh
@@ -11,13 +11,13 @@ assembly=$3
 tmpfile=${xmlfile}.tmp
 outfile=${xmlfile}.new
 
-echo "Creating the LRG annotation set and moving ${assembly} assembly mappings"
+echo "> Creating the LRG annotation set and moving ${assembly} assembly mappings" >&2 
 perl ${perldir}/add_LRG_annotation_set.pl -xmlfile ${xmlfile} -locus ${locus} -assembly ${assembly} -replace > ${tmpfile}
 echo ""
-echo "Adding Ensembl annotation set to ${xmlfile}"
+echo "> Adding Ensembl annotation set to ${xmlfile}" >&2 
 perl ${perldir}/add_ensembl_annotation.pl -xmlfile ${tmpfile} -assembly ${assembly} -registry ${REGISTRYFILE} -replace > ${outfile}
 
 rm ${tmpfile}
 
-echo "An updated LRG XML file has been written to ${outfile}"
+echo "An updated LRG XML file has been written to ${outfile}" >&2 
 
