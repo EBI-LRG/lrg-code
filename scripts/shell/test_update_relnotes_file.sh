a99e19f82f9312d33ad37637a6a7e718b8105f47
Laurent Gil <lgil@ebi.ac.uk>
2013-02-12 09:08:43 +0000

    Test script to generate the new relnotes file

---
+++ scripts/shell/test_update_relnotes_file.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+. ~/.bashrc
+. ~/.lrgpaths
+
+########
+### Update the relnotes.txt file in the CVS repository and the public FTP server
+
+# Relevant paths
+shelldir=${CVSROOTDIR}/code/scripts/shell
+
+param='test'
+tmp=$1
+
+if [ ${tmp} ]; then
+	param="${tmp} test"
+fi
+
+# Bash command
+bash ${shelldir}/update_relnotes_file.sh ${param}
