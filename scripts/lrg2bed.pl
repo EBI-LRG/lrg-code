95e62e89f646f4397b87194045703c353958378b
Laurent Gil <lgil@ebi.ac.uk>
2013-10-28 16:24:37 +0000

    Script to generate a BED file containing 6 LRG tracks: - Public genomic - Public transcript - Public protein - Pending genomic - Pending transcript - Pending protein

---
+++ scripts/lrg2bed.pl
@@ -0,0 +1,317 @@
+#! perl -w
+
+use strict;
+use LRG::LRG;
+use Getopt::Long;
+
+
+my ($xml_dir,$bed_dir,$tmp_dir,$lrg_file,$help);
+GetOptions(
+  'xml_dir=s'		=> \$xml_dir,
+  'bed_dir=s'   => \$bed_dir,
+  'tmp_dir=s'   => \$tmp_dir,
+  'xml_file=s'	=> \$lrg_file,
+	'help'        => \$help
+);
+
+die("XML directory (-xml_dir) needs to be specified!") unless (defined($xml_dir)); 
+die("Bed directory (-bed_dir) needs to be specified!") unless (defined($bed_dir)); 
+die("Temporary directory (-tmp_dir) needs to be specified!") unless (defined($tmp_dir)); 
+usage() if (defined($help));
+
+# Give write permission for the group
+umask(0002);
+
+my $separator = "\n\n\n";
+my $pending_xml_dir = "$xml_dir/pending";
+my $bed_file   = 'LRG.bed';
+my $track_name = 'LRG';
+my $track_desc = 'Locus Reference Genomic sequences (LRG)';
+
+my %lrg_meta = (
+  'public'  => {'files'      => [],
+                'dir'        => $xml_dir,
+                'colour'     => '72,167,38'},
+  'pending' => {'files'      => [],
+                'dir'        => "$xml_dir/pending",
+                'colour'     => '240,0,0'}
+);
+
+
+
+
+my @xml_list;
+my @xml_pending_list;
+if ($lrg_file) {
+	@xml_list = join(',',$lrg_file);
+}
+else {
+	opendir(DIR, $lrg_meta{'public'}{'dir'}) or die $!;
+	my @files = readdir(DIR);
+  close(DIR);
+	foreach my $file (@files) {
+	  if ($file =~ /^LRG_\d+\.xml$/) {
+		  push (@{$lrg_meta{'public'}{'files'}},$file);
+		}
+	}
+	# Pending directory
+	if (-d $lrg_meta{'pending'}{'dir'}) {
+	  opendir(DIR, $lrg_meta{'pending'}{'dir'}) or die $!;
+	  my @files = readdir(DIR);
+    close(DIR);
+	  foreach my $file (@files) {
+	    if ($file =~ /^LRG_\d+\.xml$/) {
+		    push (@{$lrg_meta{'pending'}{'files'}},$file);
+	  	}
+	  }
+	}
+}
+
+if (defined($bed_dir)) {
+  unless(-d $bed_dir){
+    mkdir $bed_dir or die "Directory $bed_dir doesn't exist and can't be created";
+  }
+}
+
+my %lrg_gene;
+my %lrg_trans;
+my %lrg_prot;
+
+my %chr_names = (
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                '10' => 10,
+                '11' => 11,
+                '12' => 12,
+                '13' => 13,
+                '14' => 13,
+                '15' => 15,
+                '16' => 16,
+                '17' => 17,
+                '18' => 18,
+                '19' => 19,
+                '20' => 20,
+                '21' => 21,
+                '22' => 22,
+                'X' => 23
+                );
+
+open BED, "> $tmp_dir/$bed_file" or die $!;
+
+##################
+# Fetch LRG data #
+##################
+foreach my $status (keys(%lrg_meta)) {
+
+  foreach my $xml (@{$lrg_meta{$status}{'files'}}) {
+	  my $lrg = LRG::LRG::newFromFile($lrg_meta{$status}{'dir'}."/$xml") or die("ERROR: Could not create LRG object from XML file!");
+	  my $lrg_id = $lrg->findNodeSingle('fixed_annotation/id')->content;
+
+    my (%diff, $chr, $chr_name, $g_start, $g_end, $l_start, $l_end, $strand);
+    
+    #	Genomic coordinates
+	  my $asets = $lrg->findNodeArraySingle('updatable_annotation/annotation_set');
+	  foreach my $aset (@$asets) {
+	    next if ($aset->findNodeSingle('source/name')->content ne "LRG");
+	    my $gene_name = $aset->findNodeSingle('lrg_locus')->content;
+	    my $mapping = $aset->findNodeSingle('mapping');
+	    
+	    $g_start = $mapping->data()->{'other_start'};
+	    $g_end   = $mapping->data()->{'other_end'};
+	   
+	    $chr      = $mapping->data()->{'other_name'};
+	    $chr_name = $chr_names{$chr};
+	    
+	    my $start   = $g_start -1; # BED: The first base in a chromosome is numbered 0
+	    my $end     = $g_end-1;     # BED: The first base in a chromosome is numbered 0
+	    
+	    my $mapping_span = $mapping->findNodeSingle('mapping_span');
+	    $strand = ($mapping_span->data()->{'strand'} == 1) ? '+' : '-';
+	    
+	    my $line_content = "chr$chr\t$start\t$end\t$lrg_id ($gene_name)\t0\t$strand";
+	  
+		  if ($lrg_gene{$status}{$chr_name}{$start}) {
+	      push(@{$lrg_gene{$status}{$chr_name}{$start}}, $line_content);
+		  }
+	    elsif ($lrg_gene{$status}{$chr_name}) {
+		    $lrg_gene{$status}{$chr_name}{$start} = [$line_content];
+		  }
+		  else {
+		    $lrg_gene{$status}{$chr_name} = {$start => [$line_content]};
+		  }
+	    
+	    $l_start = $mapping_span->data()->{'lrg_start'};
+	    $l_end   = $mapping_span->data()->{'lrg_end'};
+	  
+	    # Mapping_diff
+      foreach my $mapping_diff (@{$mapping_span->findNodeArray('diff') || []}) {
+        my $type = $mapping_diff->data->{type};
+        next if ($type eq 'mismatch');
+        my $other_start = $mapping_diff->data->{other_start};
+        my $other_end   = $mapping_diff->data->{other_end};
+        my $lrg_start   = $mapping_diff->data->{lrg_start};
+        my $lrg_end     = $mapping_diff->data->{lrg_end};
+        my $size        = ($type eq 'lrg_ins') ? $lrg_end-$lrg_start+1 :  $other_end-$other_start+1;
+
+        $diff{$other_start} = {'other_start' => $other_start,
+                               'other_end'   => $other_end,
+                               'lrg_start'   => $lrg_start,
+                               'lrg_end'     => $lrg_end,
+                               'type'        => $type,
+                               'size'        => $size };
+      }
+	  }
+	  
+	  # Transcript coordinates
+    my $transcripts = $lrg->findNodeArraySingle('fixed_annotation/transcript');
+    foreach my $transcript (@$transcripts) {
+      my $t_name   = $lrg_id.$transcript->data->{name};
+      my $t_coords = $transcript->findNodeSingle('coordinates');
+      my $t_start  = lrg2genomic($t_coords->data->{start},$l_start,$g_start,\%diff)-1;
+      my $t_end    = lrg2genomic($t_coords->data->{end},$l_start,$g_start,\%diff)-1;
+
+      my $t_line_content = "chr$chr\t$t_start\t$t_end\t$t_name ($lrg_id transcript)\t0\t$strand";
+
+      if ($lrg_trans{$status}{$chr_name}{$t_start}) {
+	      push(@{$lrg_trans{$status}{$chr_name}{$t_start}}, $t_line_content);
+		  }
+		  elsif ($lrg_trans{$status}{$chr_name}) {
+		    $lrg_trans{$status}{$chr_name}{$t_start} = [$t_line_content];
+		  }
+		  else {
+        $lrg_trans{$status}{$chr_name} = {$t_start => [$t_line_content]};
+		  }
+
+      # Protein coordinates
+      my $codings = $transcript->findNodeArraySingle('coding_region');
+      foreach my $coding (@$codings) {
+        my $p_name   = $lrg_id.$coding->findNodeSingle('translation')->data->{name};
+        my $p_coords = $coding->findNode('coordinates');
+        my $p_start  = lrg2genomic($p_coords->data->{start},$l_start,$g_start,\%diff);
+        my $p_end    = lrg2genomic($p_coords->data->{end},$l_start,$g_start,\%diff);
+        
+        my $p_line_content = "chr$chr\t$p_start\t$p_end\t$p_name ($lrg_id protein)\t0\t$strand";
+
+        if ($lrg_prot{$status}{$chr_name}{$p_start}) {
+	        push(@{$lrg_prot{$status}{$chr_name}{$p_start}}, $p_line_content);
+		    }
+		    elsif ($lrg_prot{$status}{$chr_name}) {
+		      $lrg_prot{$status}{$chr_name}{$p_start} = [$p_line_content];
+		    }
+		    else {
+		      $lrg_prot{$status}{$chr_name} = {$p_start => [$p_line_content]};
+		    }
+      }  
+	  }
+	}
+	print STDOUT "$status LRG data fetched\n";
+}	
+
+###########################################
+# Print the data/tracks into the BED file #
+###########################################
+my $priority = 1;
+foreach my $status (sort {$b cmp $a} (keys(%lrg_gene))) {
+  next if (scalar(keys(%{$lrg_gene{$status}})) == 0);
+  
+  my $track_colour = $lrg_meta{$status}{'colour'};
+  my $add_status_to_name = ($status eq 'pending') ? ' [pending]' : '';
+  
+  # Genomic track
+  print BED "track name=\"$track_name$add_status_to_name\" type=bedDetail description=\"$track_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
+  foreach my $lrg_chr (sort(keys(%{$lrg_gene{$status}}))) {
+    foreach my $lrg_start (sort(keys(%{$lrg_gene{$status}{$lrg_chr}}))) {
+      foreach my $lrg_line (@{$lrg_gene{$status}{$lrg_chr}{$lrg_start}}) {
+        print BED "$lrg_line\n";
+      }
+    }
+  }
+  print STDOUT "$status - LRG genomic data written in the BED file\n";
+  
+  # Transcript track
+  my $tr_desc = $track_desc;
+  $tr_desc =~ s/sequence/transcript/i;
+  print BED "track name=\"$track_name transcripts$add_status_to_name\" type=bedDetail description=\"$tr_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
+  foreach my $lrg_chr (sort(keys(%{$lrg_trans{$status}}))) {
+    foreach my $lrg_start (sort(keys(%{$lrg_trans{$status}{$lrg_chr}}))) {
+      foreach my $lrg_line (@{$lrg_trans{$status}{$lrg_chr}{$lrg_start}}) {
+        print BED "$lrg_line\n";
+      }
+    }
+  }
+  print STDOUT "$status - LRG transcript data written in the BED file\n";
+  $priority ++;
+  
+  # Protein track
+  my $pr_desc = $track_desc;
+  $pr_desc =~ s/sequence/protein/i;
+  print BED "track name=\"$track_name proteins$add_status_to_name\" type=bedDetail description=\"$pr_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
+  foreach my $lrg_chr (sort(keys(%{$lrg_prot{$status}}))) {
+    foreach my $lrg_start (sort(keys(%{$lrg_prot{$status}{$lrg_chr}}))) {
+      foreach my $lrg_line (@{$lrg_prot{$status}{$lrg_chr}{$lrg_start}}) {
+        print BED "$lrg_line\n";
+      }
+    }
+  }
+  print STDOUT "$status - LRG protein data written in the BED file\n";
+  $priority ++;
+}
+close(BED);
+`mv $tmp_dir/$bed_file $bed_dir/`;
+
+
+sub lrg2genomic {
+  my $coord   = shift;
+  my $l_start = shift;
+  my $g_start = shift;
+  my $diff    = shift;
+
+  my $tmp_coord = ($l_start == 1) ? $coord+$g_start-1 : $coord+$g_start-($l_start+1);
+  my $new_g_start = $g_start;  
+
+  foreach my $diff_start (sort{ $a <=> $b } keys(%$diff)) {
+    if ($diff_start <= $tmp_coord) {
+      my $size = $diff->{$diff_start}{size};
+      my $type   = $diff->{$diff_start}{type};
+ 
+      if ($type eq 'lrg_ins') { 
+        $new_g_start -= $size;
+      } else {
+        $new_g_start += $size;
+      }
+    }
+    else {
+      last;
+    }
+  }
+  return ($l_start == 1) ? $coord+$new_g_start-1 : $coord+$new_g_start-($l_start+1);
+}
+
+
+sub usage {
+    
+  print qq{
+  Usage: perl lrg2bed.pl [OPTION]
+  
+  Generate bed file(s) from LRG XML record(s)
+	
+  Options:
+    
+        -xml_dir       Path to LRG XML directory to be read (required)
+				-bed_dir       Path to LRG bed directory where the file(s) will be stored (required)
+        -xml_file      Name of the LRG XML file(s) where you want to extract the fasta sequence.
+                       If ommited, the script will extract all the LRG mapping from the XML directory.
+                       You can specify several LRG XML files by separating them with a coma:
+                       e.g. LRG_1.xml,LRG_2.xml,LRG_3.xml
+        -help          Print this message
+        
+  };
+  exit(0);
+}
