ebb181c524eb1c7d45727fe98fa342beb4465fc1
Laurent Gil <lgil@ebi.ac.uk>
2013-11-07 14:08:49 +0000

    Fix mapping issues with the reverse strand LRGs and display exons in the transcript tracks

--- scripts/lrg2bed.pl
+++ scripts/lrg2bed.pl
@@ -29,12 +29,18 @@ my $track_name = 'LRG';
 my $track_desc = 'Locus Reference Genomic sequences (LRG)';
 
 my %lrg_meta = (
-  'public'  => {'files'      => [],
-                'dir'        => $xml_dir,
-                'colour'     => '72,167,38'},
-  'pending' => {'files'      => [],
-                'dir'        => "$xml_dir/pending",
-                'colour'     => '240,0,0'}
+  'public'  => {'files'  => [],
+                'dir'    => $xml_dir,
+                'colour' => {'gene'  => '72,167,38',
+                             'trans' => '54,125,29',
+                             'prot'  => '36,84,19'}
+               },
+  'pending' => {'files'  => [],
+                'dir'    => "$xml_dir/pending",
+                'colour' => {'gene'  => '240,0,0',
+                             'trans' => '190,0,0',
+                             'prot'  => '140,0,0'}
+               }
 );
 
 
@@ -114,7 +120,7 @@ foreach my $status (keys(%lrg_meta)) {
 	  my $lrg = LRG::LRG::newFromFile($lrg_meta{$status}{'dir'}."/$xml") or die("ERROR: Could not create LRG object from XML file!");
 	  my $lrg_id = $lrg->findNodeSingle('fixed_annotation/id')->content;
 
-    my (%diff, $chr, $chr_name, $g_start, $g_end, $l_start, $l_end, $strand);
+    my (%diff, $chr, $chr_name, $g_start, $g_end, $l_start, $l_end, $strand, $strand_operator);
     
     #	Genomic coordinates
 	  my $asets = $lrg->findNodeArraySingle('updatable_annotation/annotation_set');
@@ -129,13 +135,14 @@ foreach my $status (keys(%lrg_meta)) {
 	    $chr      = $mapping->data()->{'other_name'};
 	    $chr_name = $chr_names{$chr};
 	    
-	    my $start   = $g_start -1; # BED: The first base in a chromosome is numbered 0
+	    my $start   = $g_start-1; # BED: The first base in a chromosome is numbered 0
 	    my $end     = $g_end-1;     # BED: The first base in a chromosome is numbered 0
 	    
 	    my $mapping_span = $mapping->findNodeSingle('mapping_span');
-	    $strand = ($mapping_span->data()->{'strand'} == 1) ? '+' : '-';
+	    $strand = $mapping_span->data()->{'strand'};
+	    $strand_operator = ($strand == 1) ? '+' : '-';
 	    
-	    my $line_content = "chr$chr\t$start\t$end\t$lrg_id ($gene_name)\t0\t$strand";
+	    my $line_content = "chr$chr\t$start\t$end\t$lrg_id ($gene_name)\t0\t$strand_operator";
 	  
 		  if ($lrg_gene{$status}{$chr_name}{$start}) {
 	      push(@{$lrg_gene{$status}{$chr_name}{$start}}, $line_content);
@@ -170,48 +177,110 @@ foreach my $status (keys(%lrg_meta)) {
 	  }
 	  
 	  # Transcript coordinates
+	  my %protein_list;
     my $transcripts = $lrg->findNodeArraySingle('fixed_annotation/transcript');
     foreach my $transcript (@$transcripts) {
-      my $t_name   = $lrg_id.$transcript->data->{name};
+      my $t_short_name = $transcript->data->{name};
+      my $t_name   = $lrg_id.$t_short_name;
+      my $t_number = substr($t_short_name,1);
       my $t_coords = $transcript->findNodeSingle('coordinates');
-      my $t_start  = lrg2genomic($t_coords->data->{start},$l_start,$g_start,\%diff)-1;
-      my $t_end    = lrg2genomic($t_coords->data->{end},$l_start,$g_start,\%diff)-1;
+      my $t_start  = lrg2genomic($t_coords->data->{start},$l_start,$g_start,$g_end,\%diff,$strand)-1;
+      my $t_end    = lrg2genomic($t_coords->data->{end},$l_start,$g_start,$g_end,\%diff,$strand)-1;
 
-      my $t_line_content = "chr$chr\t$t_start\t$t_end\t$t_name ($lrg_id transcript)\t0\t$strand";
+      if ($strand == -1) {
+        my $t_tmp = $t_start;
+        $t_start = $t_end;
+        $t_end = $t_tmp;
+      }
 
-      if ($lrg_trans{$status}{$chr_name}{$t_start}) {
-	      push(@{$lrg_trans{$status}{$chr_name}{$t_start}}, $t_line_content);
+      # Exons
+      my @exons_sizes; 
+      my @exons_starts;
+      my $exons = $transcript->findNodeArray('exon');
+      my $exons_count = scalar(@$exons);
+
+      foreach my $exon (@$exons) {
+        my ($e_start, $e_end, $e_size, $e_relative_start);
+        foreach my $e_coords (@{$exon->findNodeArray('coordinates')}) {
+          next if ($e_coords->data->{coord_system} ne $lrg_id);        
+
+          $e_start = lrg2genomic($e_coords->data->{start},$l_start,$g_start,$g_end,\%diff,$strand)-1;
+          $e_end   = lrg2genomic($e_coords->data->{end},$l_start,$g_start,$g_end,\%diff,$strand)-1;
+        }
+        die "Can't get genomic coordinates for an exon of the transcript $t_name" if (!defined($e_start) || !defined($e_end));
+        
+        if ($strand == -1) {
+          my $e_tmp = $e_start;
+          $e_start = $e_end;
+          $e_end = $e_tmp;
+        }
+        $e_size  = $e_end-$e_start; # No "+1" because we only want the relative coordinate of the end (e.g. chr1:1-10 has a sequence length of 10 but the block size is 9 : 1+9=10)
+        push(@exons_sizes,$e_size);
+        
+        $e_relative_start = $e_start-$t_start;
+        push(@exons_starts,$e_relative_start);
+      }
+
+      if ($strand == -1) {
+        @exons_sizes  = reverse(@exons_sizes);
+        @exons_starts = reverse(@exons_starts);
+      }
+      my $exons_sizes_list = join(',',@exons_sizes);
+      my $exons_starts_list = join(',',@exons_starts);
+      
+      my $t_line_content = "chr$chr\t$t_start\t$t_end\t$t_name\t0\t$strand_operator\t$t_start\t$t_end\t0\t$exons_count\t$exons_sizes_list\t$exons_starts_list";
+      
+      if ($lrg_trans{$status}{$chr_name}{$t_start}{$t_number}) {
+	      push(@{$lrg_trans{$status}{$chr_name}{$t_start}{$t_number}}, $t_line_content);
+	    }
+      elsif ($lrg_trans{$status}{$chr_name}{$t_start}) {
+	      $lrg_trans{$status}{$chr_name}{$t_start}{$t_number} = [$t_line_content];
 		  }
 		  elsif ($lrg_trans{$status}{$chr_name}) {
-		    $lrg_trans{$status}{$chr_name}{$t_start} = [$t_line_content];
+		    $lrg_trans{$status}{$chr_name}{$t_start} = {$t_number => [$t_line_content]};
 		  }
 		  else {
-        $lrg_trans{$status}{$chr_name} = {$t_start => [$t_line_content]};
+        $lrg_trans{$status}{$chr_name} = {$t_start => { $t_number => [$t_line_content]}};
 		  }
 
       # Protein coordinates
       my $codings = $transcript->findNodeArraySingle('coding_region');
       foreach my $coding (@$codings) {
-        my $p_name   = $lrg_id.$coding->findNodeSingle('translation')->data->{name};
-        my $p_coords = $coding->findNode('coordinates');
-        my $p_start  = lrg2genomic($p_coords->data->{start},$l_start,$g_start,\%diff);
-        my $p_end    = lrg2genomic($p_coords->data->{end},$l_start,$g_start,\%diff);
+        my $p_short_name = $coding->findNodeSingle('translation')->data->{name};
         
-        my $p_line_content = "chr$chr\t$p_start\t$p_end\t$p_name ($lrg_id protein)\t0\t$strand";
+        next if ($protein_list{$p_short_name});
+        $protein_list{$p_short_name} = 1;
+        
+        my $p_name   = $lrg_id.$p_short_name;
+        my $p_number = substr($p_short_name,1);
+        my $p_coords = $coding->findNode('coordinates');
+        my $p_start  = lrg2genomic($p_coords->data->{start},$l_start,$g_start,$g_end,\%diff,$strand);
+        my $p_end    = lrg2genomic($p_coords->data->{end},$l_start,$g_start,$g_end,\%diff,$strand);
+        
+        if ($strand == -1) {
+          my $p_tmp = $p_start;
+          $p_start = $p_end;
+          $p_end = $p_tmp;
+        }
+        
+        my $p_line_content = "chr$chr\t$p_start\t$p_end\t$p_name ($lrg_id protein $p_number)\t0\t$strand_operator";
 
-        if ($lrg_prot{$status}{$chr_name}{$p_start}) {
-	        push(@{$lrg_prot{$status}{$chr_name}{$p_start}}, $p_line_content);
+        if ($lrg_prot{$status}{$chr_name}{$p_start}{$p_number}) {
+	        push(@{$lrg_prot{$status}{$chr_name}{$p_start}{$p_number}}, $p_line_content);
+	      }
+        elsif ($lrg_prot{$status}{$chr_name}{$p_start}) {
+	        $lrg_prot{$status}{$chr_name}{$p_start}{$p_number} = [$p_line_content];
 		    }
 		    elsif ($lrg_prot{$status}{$chr_name}) {
-		      $lrg_prot{$status}{$chr_name}{$p_start} = [$p_line_content];
+		      $lrg_prot{$status}{$chr_name}{$p_start} = {$p_number => [$p_line_content]};
 		    }
 		    else {
-		      $lrg_prot{$status}{$chr_name} = {$p_start => [$p_line_content]};
+          $lrg_prot{$status}{$chr_name} = {$p_start => { $p_number => [$p_line_content]}};
 		    }
       }  
 	  }
 	}
-	print STDOUT "$status LRG data fetched\n";
+	print STDOUT "LRG DATA: $status LRG data fetched\n";
 }	
 
 ###########################################
@@ -221,46 +290,55 @@ my $priority = 1;
 foreach my $status (sort {$b cmp $a} (keys(%lrg_gene))) {
   next if (scalar(keys(%{$lrg_gene{$status}})) == 0);
   
-  my $track_colour = $lrg_meta{$status}{'colour'};
+  my $track_colour = $lrg_meta{$status}{'colour'}{'gene'};
   my $add_status_to_name = ($status eq 'pending') ? ' [pending]' : '';
   
   # Genomic track
   print BED "track name=\"$track_name$add_status_to_name\" type=bedDetail description=\"$track_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
-  foreach my $lrg_chr (sort(keys(%{$lrg_gene{$status}}))) {
-    foreach my $lrg_start (sort(keys(%{$lrg_gene{$status}{$lrg_chr}}))) {
+  foreach my $lrg_chr (sort {$a <=> $b} keys(%{$lrg_gene{$status}})) {
+    foreach my $lrg_start (sort {$a <=> $b} keys(%{$lrg_gene{$status}{$lrg_chr}})) {
       foreach my $lrg_line (@{$lrg_gene{$status}{$lrg_chr}{$lrg_start}}) {
         print BED "$lrg_line\n";
       }
     }
   }
-  print STDOUT "$status - LRG genomic data written in the BED file\n";
+  print STDOUT "BED FILE: $status - LRG genomic data written in the BED file\n";
   
   # Transcript track
+  $track_colour = $lrg_meta{$status}{'colour'}{'trans'};
+  
   my $tr_desc = $track_desc;
   $tr_desc =~ s/sequence/transcript/i;
-  print BED "track name=\"$track_name transcripts$add_status_to_name\" type=bedDetail description=\"$tr_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
-  foreach my $lrg_chr (sort(keys(%{$lrg_trans{$status}}))) {
-    foreach my $lrg_start (sort(keys(%{$lrg_trans{$status}{$lrg_chr}}))) {
-      foreach my $lrg_line (@{$lrg_trans{$status}{$lrg_chr}{$lrg_start}}) {
-        print BED "$lrg_line\n";
-      }
+  
+  print BED "track name=\"$track_name transcripts$add_status_to_name\" description=\"$tr_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
+  foreach my $lrg_chr (sort {$a <=> $b} keys(%{$lrg_trans{$status}})) {
+    foreach my $lrg_start (sort {$a <=> $b} keys(%{$lrg_trans{$status}{$lrg_chr}})) {
+      foreach my $t_number (sort {$a <=> $b} keys(%{$lrg_trans{$status}{$lrg_chr}{$lrg_start}})) {
+        foreach my $lrg_line (@{$lrg_trans{$status}{$lrg_chr}{$lrg_start}{$t_number}}) {
+          print BED "$lrg_line\n";
+        }
+      }  
     }
   }
-  print STDOUT "$status - LRG transcript data written in the BED file\n";
+  print STDOUT "BED FILE: $status - LRG transcript data written in the BED file\n";
   $priority ++;
   
   # Protein track
+  $track_colour = $lrg_meta{$status}{'colour'}{'prot'};
+  
   my $pr_desc = $track_desc;
   $pr_desc =~ s/sequence/protein/i;
   print BED "track name=\"$track_name proteins$add_status_to_name\" type=bedDetail description=\"$pr_desc - $status\" color=$track_colour priority=$priority db=hg19 visibility=3 url=\"http://www.lrg-sequence.org/\"\n";
-  foreach my $lrg_chr (sort(keys(%{$lrg_prot{$status}}))) {
-    foreach my $lrg_start (sort(keys(%{$lrg_prot{$status}{$lrg_chr}}))) {
-      foreach my $lrg_line (@{$lrg_prot{$status}{$lrg_chr}{$lrg_start}}) {
-        print BED "$lrg_line\n";
-      }
+  foreach my $lrg_chr (sort {$a <=> $b} keys(%{$lrg_prot{$status}})) {
+    foreach my $lrg_start (sort {$a <=> $b} keys(%{$lrg_prot{$status}{$lrg_chr}})) {
+      foreach my $p_number (sort {$a <=> $b} keys(%{$lrg_prot{$status}{$lrg_chr}{$lrg_start}})) {
+        foreach my $lrg_line (@{$lrg_prot{$status}{$lrg_chr}{$lrg_start}{$p_number}}) {
+          print BED "$lrg_line\n";
+        }
+      }  
     }
   }
-  print STDOUT "$status - LRG protein data written in the BED file\n";
+  print STDOUT "BED FILE: $status - LRG protein data written in the BED file\n";
   $priority ++;
 }
 close(BED);
@@ -271,27 +349,53 @@ sub lrg2genomic {
   my $coord   = shift;
   my $l_start = shift;
   my $g_start = shift;
+  my $g_end   = shift;
   my $diff    = shift;
+  my $strand  = shift;
 
-  my $tmp_coord = ($l_start == 1) ? $coord+$g_start-1 : $coord+$g_start-($l_start+1);
-  my $new_g_start = $g_start;  
-
-  foreach my $diff_start (sort{ $a <=> $b } keys(%$diff)) {
-    if ($diff_start <= $tmp_coord) {
-      my $size = $diff->{$diff_start}{size};
-      my $type   = $diff->{$diff_start}{type};
+  my ($tmp_coord, $new_g_start);
+  if ($strand == -1) {
+    $tmp_coord = $g_end-$coord+$l_start;
+    my $new_g_end = $g_end; 
+    
+    foreach my $diff_start (sort{ $a <=> $b } keys(%$diff)) {
+      if ($diff_start >= $tmp_coord) {
+        my $size = $diff->{$diff_start}{size};
+        my $type = $diff->{$diff_start}{type};
  
-      if ($type eq 'lrg_ins') { 
-        $new_g_start -= $size;
-      } else {
-        $new_g_start += $size;
+        if ($type eq 'lrg_ins') { 
+          $new_g_end -= $size;
+        } else {
+          $new_g_end += $size;
+        }
+      }
+      else {
+        last;
       }
     }
-    else {
-      last;
-    }
+    return $new_g_end-$coord+$l_start;;
   }
-  return ($l_start == 1) ? $coord+$new_g_start-1 : $coord+$new_g_start-($l_start+1);
+  else {
+    $tmp_coord = $coord+$g_start-$l_start;
+    $new_g_start = $g_start; 
+
+    foreach my $diff_start (sort{ $a <=> $b } keys(%$diff)) {
+      if ($diff_start <= $tmp_coord) {
+        my $size = $diff->{$diff_start}{size};
+        my $type = $diff->{$diff_start}{type};
+ 
+        if ($type eq 'lrg_ins') { 
+          $new_g_start -= $size;
+        } else {
+          $new_g_start += $size;
+        }
+      }
+      else {
+        last;
+      }
+    }
+    return $coord+$new_g_start-$l_start;
+  }  
 }
 
 
