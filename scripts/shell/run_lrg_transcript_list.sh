bdda1a1d563b8e13313d04cea58bc1506d0e1760
Laurent Gil <lgil@ebi.ac.uk>
2013-11-14 11:46:26 +0000

    Add a temporary directory option

--- scripts/shell/run_lrg_transcript_list.sh
+++ scripts/shell/run_lrg_transcript_list.sh
@@ -7,4 +7,9 @@ if [[ $1 ]]; then
   path="-xml_dir $1"
 fi
 
-perl ${CVSROOTDIR}/code/scripts/get_lrg_transcript_list.pl ${path}
+tmpdir="-tmp_dir ${CVSROOTDIR}"
+if [[ $2 ]]; then
+  tmpdir="-tmp_dir $2"
+fi
+
+perl ${CVSROOTDIR}/code/scripts/get_lrg_transcript_list.pl ${path} ${tmpdir}
