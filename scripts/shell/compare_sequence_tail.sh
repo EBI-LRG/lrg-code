c0af043ca10b25211d42d77443dae0d261cea9cf
Laurent Gil <lgil@ebi.ac.uk>
2013-12-19 10:29:23 +0000

    Add an option so the script can be 'silent'

--- scripts/shell/compare_sequence_tail.sh
+++ scripts/shell/compare_sequence_tail.sh
@@ -12,9 +12,14 @@ perldir=${CVSROOTDIR}/code/scripts/
 
 xmlfile=$1
 error_log=$2
+silent=$3
 
 # Use the value "none" for warning, if you want to skip the check of the polyA between the RefSeq transcript sequences and the LRG transcript sequences
-
-echo "Compare ${lrgid} transcripts tail sequences with RefSeq sequences..."
-perl ${perldir}/compare_sequence_tail.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -xmlfile ${xmlfile} -verbose -error_log ${error_log}
+if [[ ${silent} ]]; then
+  verbose=''
+else 
+  echo "Compare ${lrgid} transcripts tail sequences with RefSeq sequences..."
+  verbose='-verbose'
+fi  
+perl ${perldir}/compare_sequence_tail.pl -host ${host} -user ${user} -pass ${pass} -port ${port} -dbname ${dbname} -xmlfile ${xmlfile} ${verbose} -error_log ${error_log}
 
