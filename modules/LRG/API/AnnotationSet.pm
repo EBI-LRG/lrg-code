16f1cf588503d88118503d55be939156bf52331a
Laurent Gil <lgil@ebi.ac.uk>
2014-10-07 14:12:05 +0100

    Cleanup the module

--- modules/LRG/API/AnnotationSet.pm
+++ modules/LRG/API/AnnotationSet.pm
@@ -52,45 +52,4 @@ sub remove_lrg_locus {
   return $self->_remove_meta('lrg_locus');
 }
 
-#sub _meta {
-#  my $self = shift;
-#  my $key = shift;
-#  my $value = shift;
-#  
-#  # Locate the meta object that contains the key
-#  my $meta;
-#  my @keep;
-#  foreach my $m (@{$self->meta() || []}) {
-#    unless ($m->key() eq $key) {
-#      push(@keep,$m);
-#      next;
-#    }
-#    $meta = $m;
-#  }
-#  # If we're not updating the value, return what we did or did not find
-#  return $meta unless (defined($value));
-#  
-#  # Update the meta with the pre-existing and the new value
-#
-#  $self->meta([@keep,LRG::API::Meta->new($key,$value)]);
-#  #$self->meta([@keep,$value]);
-#
-#  return $value;
-#}
-
-#sub _remove_meta {
-#  my $self = shift;
-#  my $key = shift;
-#  
-#  # Locate the meta object that contains the key
-#  my @keep;
-#  foreach my $m (@{$self->meta() || []}) {
-#    next if ($m->key() eq $key);
-#    push(@keep,$m);
-#  }
-#  
-#  # Update the meta with the existing list except the removed metadata
-#  $self->meta(\@keep);
-#}
-
 1;
