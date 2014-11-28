0fa26248df0098291921bf2140ccf8178fe51164
Laurent Gil <lgil@ebi.ac.uk>
2014-11-28 14:14:14 +0000

    New option '-ftp_dir' for the report2html.pl script + notes for the next steps of the pipeline

--- scripts/shell/run_all_automated_pipelines.sh
+++ scripts/shell/run_all_automated_pipelines.sh
@@ -2,6 +2,7 @@
 . ~/.bashrc
 . ~/.lrgpaths
 
+pubpath=${PUBFTP}
 
 xml_dir=$1
 new_dir=$2
@@ -24,10 +25,23 @@ fi
 perldir=${CVSROOTDIR}/lrg-code/scripts/auto_pipeline
 
 # Extract the XML files 
-perl ${perldir}/get_data_files.pl -xml_tmp_dir ${xml_dir}
+#perl ${perldir}/get_data_files.pl -xml_tmp_dir ${xml_dir}
 
 # Run the pipeline for each LRG XML file
 perl ${perldir}/pipeline_dispatcher.pl -ncbi_xml_dir ${xml_dir}/${xml_dir_sub} -new_xml_dir ${new_dir} -reports_dir ${tmp_dir} -reports_file ${reports_file} ${is_test_hc}
 
 # Generate an HTML output of the pipeline reports
-perl ${perldir}/reports2html.pl -reports_dir ${tmp_dir} -reports_file ${reports_file} -xml_dir ${new_dir}
+perl ${perldir}/reports2html.pl -reports_dir ${tmp_dir} -reports_file ${reports_file} -xml_dir ${new_dir} -ftp_dir ${pubpath}
+
+# Update xml CVS directory with the updated LRG XML files. 
+
+# Generate index files
+
+# Copy LRG XML, fasta, gff and index XML files to the LRG FTP
+
+# Run the script updates_relnotes_file.sh
+
+
+
+
+
