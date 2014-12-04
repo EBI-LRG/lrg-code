a6f1a0bcdb035f6d4089d86016278064baeefccb
Laurent Gil <lgil@ebi.ac.uk>
2014-12-04 10:52:24 +0000

    Add new steps to the automated pipeline

---
+++ modules/LRG/Pipeline/UpdateRelnotesFile.pm
@@ -0,0 +1,19 @@
+package LRG::Pipeline::UpdateRelnotesFile;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Hive::Process');
+
+sub run {
+  my $self = shift;
+  
+  my $run_dir    = $self->param('run_dir');
+  my $assembly   = $self->param('assembly');
+  my $tmp_dir    = $self->param('new_dir');
+  my $is_test    = ($self->param('is_test') == 1) ? ' 1' : '';
+  my $date       = $self->param('date');
+ 
+  `bash $run_dir/lrg-code/scripts/shell/update_relnotes_file.sh $assembly $tmp_dir/$date/tmp$is_test`;
+}
+1;
