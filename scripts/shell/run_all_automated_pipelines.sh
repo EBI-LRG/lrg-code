23547eab8401ddc09e38cce133a2e3024eb74dac
Laurent Gil <lgil@ebi.ac.uk>
2014-12-04 11:02:48 +0000

    Change the script so it will be ran by the ehive

--- scripts/shell/run_all_automated_pipelines.sh
+++ scripts/shell/run_all_automated_pipelines.sh
@@ -2,46 +2,15 @@
 . ~/.bashrc
 . ~/.lrgpaths
 
-pubpath=${PUBFTP}
 
-xml_dir=$1
-new_dir=$2
-tmp_dir=$3
-is_test_hc=$4      # Test the Ensembl annotations
+# Init the pipeline
+init_pipeline.pl LRG::Pipeline::Pipeline_conf
 
-xml_dir_sub='xml'
-reports_file='pipeline_reports.txt'
+db_name='lrg_automated_pipeline'
+beekeeper="beekeeper.pl -url mysql://admin:${LRGDBPASS}@${LRGDBHOST}:${LRGDBPORT}/${db_name}"
 
-if [[ -n ${is_test_hc} && ${is_test_hc} != 0 ]] ; then
-  if [[ ${is_test_hc} == 'is_hc' ]]; then
-    is_test_hc='-is_hc'
-  else
-    is_test_hc='-is_test'
-  fi
-else
-  is_test_hc=''
-fi
+# Sync the pipeline
+${beekeeper} -sync
 
-perldir=${CVSROOTDIR}/lrg-code/scripts/auto_pipeline
-
-# Extract the XML files 
-#perl ${perldir}/get_data_files.pl -xml_tmp_dir ${xml_dir}
-
-# Run the pipeline for each LRG XML file
-perl ${perldir}/pipeline_dispatcher.pl -ncbi_xml_dir ${xml_dir}/${xml_dir_sub} -new_xml_dir ${new_dir} -reports_dir ${tmp_dir} -reports_file ${reports_file} ${is_test_hc}
-
-# Generate an HTML output of the pipeline reports
-perl ${perldir}/reports2html.pl -reports_dir ${tmp_dir} -reports_file ${reports_file} -xml_dir ${new_dir} -ftp_dir ${pubpath}
-
-# Update xml CVS directory with the updated LRG XML files. 
-
-# Generate index files
-
-# Copy LRG XML, fasta, gff and index XML files to the LRG FTP
-
-# Run the script updates_relnotes_file.sh
-
-
-
-
-
+# Run the pipeline
+${beekeeper} -loop
