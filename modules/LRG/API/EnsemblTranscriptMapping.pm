16bbd67b8d4b9e185f9e79c9c92e19c4c152571b
Laurent Gil <lgil@ebi.ac.uk>
2015-04-02 16:36:47 +0100

    Remove tabulations

--- modules/LRG/API/EnsemblTranscriptMapping.pm
+++ modules/LRG/API/EnsemblTranscriptMapping.pm
@@ -12,59 +12,59 @@ sub initialize {
   my $self = shift;
   my ($registry,$lrg_id,$gene,$diffs) = @_;
 
-	unless (defined($registry)) {
+  unless (defined($registry)) {
     die ("Registry must be specified in call to $self constructor");
   }
-	unless (defined($lrg_id)) {
+  unless (defined($lrg_id)) {
     die ("LRG ID must be specified in call to $self constructor");
   }
-	unless (defined($gene)) {
+  unless (defined($gene)) {
     die ("Gene must be specified in call to $self constructor");
   }
-	
-	my $slice_adaptor = $registry->get_adaptor('human','core','slice');
-	my $lrg_slice = $slice_adaptor->fetch_by_region('LRG',$lrg_id);
 
-	$self->lrg_slice($lrg_slice, 'Bio::EnsEMBL::LRGSlice') if ($lrg_slice);
-	$self->tr_adaptor($registry->get_adaptor('human','core','transcript'));
+  my $slice_adaptor = $registry->get_adaptor('human','core','slice');
+  my $lrg_slice = $slice_adaptor->fetch_by_region('LRG',$lrg_id);
+
+  $self->lrg_slice($lrg_slice, 'Bio::EnsEMBL::LRGSlice') if ($lrg_slice);
+  $self->tr_adaptor($registry->get_adaptor('human','core','transcript'));
   $self->ex_adaptor($registry->get_adaptor('human','core','exon'));
-	$self->gene($gene, 'LRG::API::GeneUp');
-	$self->diffs($diffs);
-	
+  $self->gene($gene, 'LRG::API::GeneUp');
+  $self->diffs($diffs);
+
 }
 
 sub _permitted {
   return [
     'lrg_slice',
-		'tr_adaptor',
+    'tr_adaptor',
     'ex_adaptor',
-		'gene',
+    'gene',
     'diffs'
   ];
 }
 
 sub get_transcripts_mappings {
-	my $self = shift;
-	my $gene = $self->gene;
-	my $ens_mapping;
+  my $self = shift;
+  my $gene = $self->gene;
+  my $ens_mapping;
   my $diffs_list = $self->diffs;
   my @diffs_start = keys(%$diffs_list);
 
-	foreach my $t (@{$gene->transcript}) {
-		my $tr_other;
-		my $tr_assembly;
-		my $tr_span;
-		
-		# Mapping tag	
+  foreach my $t (@{$gene->transcript}) {
+    my $tr_other;
+    my $tr_assembly;
+    my $tr_span;
+
+    # Mapping tag
     my $t_acc = $t->accession;
        $t_acc =~ /^(.+)\./;
        $t_acc = ($1) ? $1 : $t_acc;
     my $enst = $self->tr_adaptor->fetch_by_stable_id($t_acc);
-		my $tr_accession = $t_acc.".".$enst->version; 
+    my $tr_accession = $t_acc.".".$enst->version; 
 
-		foreach my $tc (@{$t->coordinates}) {
-			if ($tc->coordinate_system !~ /LRG/) {
-				$tr_other = LRG::API::Coordinates->new( $tr_accession,
+    foreach my $tc (@{$t->coordinates}) {
+      if ($tc->coordinate_system !~ /LRG/) {
+        $tr_other = LRG::API::Coordinates->new( $tr_accession,
                                                 $tc->start,     
                                                 $tc->end,
                                                 $tc->strand,
@@ -75,27 +75,27 @@ sub get_transcripts_mappings {
                                                 undef,
                                                 'other_'
                                               );
-			}
-		}
+      }
+    }
 
-		# Mapping span tag
-		my $cdna_start;
-		my $cdna_end = 1;
+    # Mapping span tag
+    my $cdna_start;
+    my $cdna_end = 1;
 
-		foreach my $e (@{$t->exon}) {	
-			my @diff_tags;
-			my $l_coord;
-			my $o_coord;
-			foreach my $ec (@{$e->coordinates}) {
-				if ($ec->coordinate_system =~ /LRG/) {
-					$l_coord = $ec;
-					$l_coord->prefix('lrg_');
-				} 
-				else {
-					my $ense = $self->ex_adaptor->fetch_by_stable_id($e->accession);
-					my @coords = $enst->genomic2cdna($ense->start,$ense->end,$ense->strand);
-					foreach my $ce (@coords) {        
-						$o_coord = LRG::API::Coordinates->new( $e->accession,
+    foreach my $e (@{$t->exon}) {
+      my @diff_tags;
+      my $l_coord;
+      my $o_coord;
+      foreach my $ec (@{$e->coordinates}) {
+        if ($ec->coordinate_system =~ /LRG/) {
+          $l_coord = $ec;
+          $l_coord->prefix('lrg_');
+        }
+        else {
+          my $ense = $self->ex_adaptor->fetch_by_stable_id($e->accession);
+          my @coords = $enst->genomic2cdna($ense->start,$ense->end,$ense->strand);
+          foreach my $ce (@coords) {
+            $o_coord = LRG::API::Coordinates->new( $e->accession,
                                                    $ce->start,
                                                    $ce->end,
                                                    1,
@@ -107,18 +107,18 @@ sub get_transcripts_mappings {
                                                    'other_'
                                                  );
 
-						$cdna_start = $ce->start if (!$cdna_start);
-						$cdna_start = $ce->start if ($cdna_start > $ce->start);
-						$cdna_end = $ce->end if ($cdna_end < $ce->end);
-					}
-				}
-			}
-			my $e_span = LRG::API::MappingSpan->new($l_coord,$o_coord,$l_coord->strand);
-			
-			# Diff tag
-			foreach my $d_start (@diffs_start) {
-				if ($d_start > $l_coord->start && $d_start < $l_coord->end) {
-						my $mapping_diff = $diffs_list->{$d_start};
+            $cdna_start = $ce->start if (!$cdna_start);
+            $cdna_start = $ce->start if ($cdna_start > $ce->start);
+            $cdna_end = $ce->end if ($cdna_end < $ce->end);
+          }
+        }
+      }
+      my $e_span = LRG::API::MappingSpan->new($l_coord,$o_coord,$l_coord->strand);
+
+      # Diff tag
+      foreach my $d_start (@diffs_start) {
+        if ($d_start > $l_coord->start && $d_start < $l_coord->end) {
+            my $mapping_diff = $diffs_list->{$d_start};
             my $moc = $mapping_diff->other_coordinates;
             my $other_coordinates = LRG::API::Coordinates->new($moc->coordinate_system,
                                                                $moc->start,
@@ -126,24 +126,24 @@ sub get_transcripts_mappings {
                                                                $moc->strand);                                   
             $other_coordinates->prefix('other_');
 
-						my @coords = $enst->genomic2cdna($other_coordinates->start,$other_coordinates->end,$o_coord->strand);
-						$other_coordinates->start($coords[0]->start);
-						$other_coordinates->end($coords[0]->end);
-						my $new_mapping_diff = LRG::API::MappingDiff->new($mapping_diff->type,
+            my @coords = $enst->genomic2cdna($other_coordinates->start,$other_coordinates->end,$o_coord->strand);
+            $other_coordinates->start($coords[0]->start);
+            $other_coordinates->end($coords[0]->end);
+            my $new_mapping_diff = LRG::API::MappingDiff->new($mapping_diff->type,
                                                               $mapping_diff->lrg_coordinates,
                                                               $other_coordinates,
                                                               $mapping_diff->lrg_sequence,
                                                               $mapping_diff->other_sequence);
-						push(@diff_tags,$new_mapping_diff);
-				}
-			}
-			$e_span->mapping_diff(\@diff_tags) if (scalar @diff_tags);
-			push (@$tr_span, $e_span); 
-		}
-		$tr_other->start($cdna_start);
-		$tr_other->end($cdna_end);
-		my $tr_mapping = LRG::API::Mapping->new($tr_accession,$mapping_type,$tr_other,$tr_span);
-		push (@$ens_mapping,$tr_mapping);
-	}
-	return $ens_mapping;
+            push(@diff_tags,$new_mapping_diff);
+        }
+      }
+      $e_span->mapping_diff(\@diff_tags) if (scalar @diff_tags);
+      push (@$tr_span, $e_span);
+    }
+    $tr_other->start($cdna_start);
+    $tr_other->end($cdna_end);
+    my $tr_mapping = LRG::API::Mapping->new($tr_accession,$mapping_type,$tr_other,$tr_span);
+    push (@$ens_mapping,$tr_mapping);
+  }
+  return $ens_mapping;
 }
