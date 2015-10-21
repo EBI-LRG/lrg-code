7ae47e98969d9a0ee7866ae2b3e26fb072bf54db
Laurent Gil <lgil@ebi.ac.uk>
2015-10-21 16:49:40 +0100

    Remove the existing HTML files before running the script

--- scripts/shell/run_alignments.sh
+++ scripts/shell/run_alignments.sh
@@ -8,4 +8,8 @@ output_dir=$2
 
 perldir=${CVSROOTDIR}/lrg-code/scripts/align
 
+if [ -d "${output_dir}" && ${output_dir} != "" ] ; then
+  rm -f "${output_dir}/*.html"
+fi
+
 perl ${perldir}/generate_transcript_alignments.pl -xml_dirs ${xml_dirs} -output_dir ${output_dir}
