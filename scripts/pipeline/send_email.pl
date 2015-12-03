2e358ed6b05f60de82cfbe53b5cde110ae329333
Laurent Gil <lgil@ebi.ac.uk>
2015-12-03 16:05:14 +0000

    Script sending automatic emails, fetching the contact information from the LRG XML files

---
+++ scripts/pipeline/send_email.pl
@@ -0,0 +1,139 @@
+use strict;
+use LRG::LRG;
+use LRG::LRGEmail;
+use LRG::API::XMLA::XMLAdaptor;
+use Getopt::Long;
+
+my $lrg_id;
+my $xml_dir;
+my $ftp_dir;
+my $status;
+my $debug;
+
+GetOptions(
+  'lrg_id=s'  => \$lrg_id, # Separated by a ','
+  'xml_dir=s' => \$xml_dir,
+  'ftp_dir=s' => \$ftp_dir,
+  'status=s'  => \$status,
+  'debug!'    => \$debug,
+);
+die("You need to specify a LRG ID (-lrg_id)") unless ($lrg_id);
+die("You need to specify a LRG status (-status)") unless ($status);
+
+$xml_dir ||= '/ebi/ftp/pub/databases/lrgex/';
+$ftp_dir ||= 'http://ftp.ebi.ac.uk/pub/databases/lrgex/';
+
+my $pending_status = 'pending';
+my $default_name   = 'LRG requester';
+my $title          = 'Dear %s,';
+my $lrg_signature  = 'The LRG team';
+
+
+my %email_subject = (
+                      'public'  => 'The LRG entry for %s (%s) has been made public',
+                      'pending' => 'The LRG entry for %s has been created (ID: %s)'
+                    );
+my %email_message = (
+                      'public' => $title.'<br />'.
+                                  '<p>'.
+                                  'We are please to announce that the LRG for %s (%s) has been made public.<br />'.
+                                  'The record is available at <a href="%s">%s</a>'.
+                                  '</p>'.
+                                  'Best regards,<br />'.
+                                  '%s',
+                      'pending' => $title.'<br />'.
+                                  '<p>'.
+                                  'We are please to announce that the LRG for %s (%s) has been created.<br />'.
+                                  'The record is available at <a href="%s">%s</a><br />'.
+                                  'This LRG record is pending approval and subject to change. Please do not use until it has passed final approval'.
+                                  '</p>'.
+                                  'Best regards,<br />'.
+                                  '%s',
+                    );
+
+
+$status = lc($status);
+
+if ($status eq $pending_status && $xml_dir !~ /$pending_status\/?$/) {
+  $xml_dir .= ($xml_dir =~ /\/$/) ? $pending_status : "/$pending_status";
+}
+if ($status eq $pending_status && $ftp_dir !~ /$pending_status\/?$/) {
+  $ftp_dir .= ($ftp_dir =~ /\/$/) ? $pending_status : "/$pending_status";
+}
+
+# Load the XML file using the API
+my $xmla = LRG::API::XMLA::XMLAdaptor->new();
+$xmla->load_xml_file("$xml_dir/$lrg_id.xml");
+
+# Get an LRGXMLAdaptor and fetch the LRG object
+my $lrg_adaptor = $xmla->get_LocusReferenceXMLAdaptor();
+my $lrg_obj = $lrg_adaptor->fetch();
+
+# Get data
+my $upd_annotation = $lrg_obj->updatable_annotation;
+
+my $hgnc;
+foreach my $aset (@{$upd_annotation->annotation_set}) {
+  if ($aset->lrg_locus) {
+    $hgnc = $aset->lrg_locus->value;
+    last;
+  }
+}
+die("Can't find a HGNC symbol for $lrg_id") unless($hgnc);
+
+my $requester_set = $lrg_obj->updatable_annotation->requester();
+
+my $has_contact = 0;
+my $has_email   = 0;
+foreach my $source (@{$requester_set->source}) {
+  foreach my $contact (@{$source->contact}) {
+    $has_contact = 1;
+    my $name  = $contact->name;
+    my $email = $contact->email;
+ 
+    $name ||= $default_name;
+
+    if ($email && $email ne '') {
+      send_lrg_email($name,$email);
+      $has_email = 1;
+    }
+  }
+}
+if ($has_contact == 0) {
+  die("No requester found for the $lrg_id. We can't send a notification email.");
+}
+elsif ($has_email == 0) {
+  die("No valid email found amongst the requesters for the $lrg_id. We can't send a notification email.");
+}
+
+
+sub send_lrg_email {
+  my $name = shift;
+  my $email = shift;
+
+  my $ftp_path = $ftp_dir;
+     $ftp_path .= '/' if ($ftp_dir !~ /\/$/);
+     $ftp_path .= "$lrg_id.xml";
+
+  # Build subject
+  my $tmp_subject = $email_subject{$status};
+  my $subject = sprintf($tmp_subject, $hgnc, $lrg_id);
+
+  # Build message content
+  my $tmp_message = $email_message{$status};
+  my $message = sprintf($tmp_message, $name, $hgnc, $lrg_id, $ftp_path, $ftp_path, $lrg_signature);
+
+
+  # Send email
+  if ($debug) {
+    print "RECIPIENT: $email\n";
+    print "SUBJECT: $subject\n";
+    print "MESSAGE:\n$message\n";
+  }
+  else {
+    #my $email_obj = LRG::LRGEmail->new($email,$subject,$message);
+    #$email_obj->send;
+  }
+}
+
+
