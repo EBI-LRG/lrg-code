7f6fb65efde253e99cf5e1aa6fb8752ad9d96049
Laurent Gil <lgil@ebi.ac.uk>
2016-06-13 13:21:53 +0100

    Add some conditions and tests before copying the file

--- scripts/shell/copy_json_index_to_website.sh
+++ scripts/shell/copy_json_index_to_website.sh
@@ -2,7 +2,13 @@
 . ~/.bashrc
 . ~/.lrgpaths
  
+index_file=${PUBFTP}/.lrg_index/lrg_index.json 
+ 
 become ${WEBADMIN} bash << EOF
-cp ${PUBFTP}/.lrg_index/lrg_index.json ${DEVWEBSITE}/json_index/
+if [ -f ${index_file} ]; then
+  if [ -s ${index_file} ]; then
+    cp ${index_file} ${DEVWEBSITE}/json_index/
+  fi
+fi
 exit
 EOF
