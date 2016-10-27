beb65b34236a3133c222e443e62a4e56bf383828
Laurent Gil <lgil@ebi.ac.uk>
2016-10-27 11:59:39 +0100

    Fix empty backups

--- scripts/shell/dump_all_lrg_tables.sh
+++ scripts/shell/dump_all_lrg_tables.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 . ~/.bashrc
 . ~/.lrgpaths
-. ~/.lrgpass
 
 # Relevant paths
 path=$1
-pass=${LRGDBPASS}
 
 if [[ ${path} ]] ; then
-  mysqldump -h ${LRGDBHOST} -P ${LRGDBPORT} -u ${LRGDBADMUSER} -p${pass} lrg | gzip > "$1/lrg_dump_"`date +%d`.sql.gz
+  /nfs/dbtools/mysql/5.6/usr/bin/mysqldump -h ${LRGDBHOST} -P ${LRGDBPORT} -u ${LRGDBROUSER} ${LRGDBNAME} --lock-tables=false | gzip > "$1/lrg_dump_"`date +%d`.sql.gz
 else
-  echo "No directory given as argument!"
+  echo "No directory given as argument for the MySQL dump script!"
 fi
 
