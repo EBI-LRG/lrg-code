f9bf317b48d7d9157158e35bc827a3e5a5cde469
Laurent Gil <lgil@ebi.ac.uk>
2017-02-10 10:07:09 +0000

    Update the scripts to populate the virtual machines

--- scripts/shell/populate_virtual_machines.sh
+++ scripts/shell/populate_virtual_machines.sh
@@ -2,7 +2,8 @@
 . ~/.bashrc
 . ~/.lrgpaths
 
-input_dir=$1 
+input_dir=$1
+prod_only=$2
 
 if [[ -z ${input_dir} ]]; then
   echo "No input dir defined!"
@@ -12,22 +13,35 @@ fi
 json_file='lrg_index.json'
 json_dir='json_index'
 
+perldir=${LRGROOTDIR}/lrg-code/scripts/
+
+# Generate the progress status page and store it into the input_dir
+#bash ${perldir}/shell/get_lrg_step_status.sh ${input_dir}/curation-status/index.html 2
+# DEV version
+bash ${perldir}/shell/get_lrg_step_status_test.sh ${input_dir}/curation-status/index.html 2
+
 become ${WEBADMIN} bash << EOF
 
-  # DEV website
-  cd ${DEVWEBSITE}
-  rm -rf ./*
-  cp -R ${input_dir}/* ./
-  cp ${PUBFTP}/.lrg_index/${json_file} ./${json_dir}/
-
-  # Fallback website
-  cd ${FBWEBSITE}
-  rm -rf ./*
-  cp -R ${input_dir}/* ./
-  cp ${PUBFTP}/.lrg_index/${json_file} ./${json_dir}/
+  if [ -z "$prod_only" ] || [ $prod_only == 2 ];then
+    # DEV website
+    cd ${DEVWEBSITE}
+    rm -rf ./*
+    cp -R ${input_dir}/* ./
+    cp ${PUBFTP}/.lrg_index/${json_file} ./${json_dir}/
+    echo "Copy to the DEV website done"
+  fi
   
-  # PROD website
-  scp -r ${FBWEBSITE}* ${WEBADMIN}@${PRODVM}:${PRODWEBSITE}/ 1> /dev/null
+  if  [ -z "$prod_only" ] || [ $prod_only == 1 ];then
+    # Fallback website
+    scd ${FBWEBSITE}
+    rm -rf ./*
+    cp -R ${input_dir}/* ./
+    cp ${PUBFTP}/.lrg_index/${json_file} ./${json_dir}/
   
+    # PROD website
+    scp -r ${FBWEBSITE}* ${WEBADMIN}@${PRODVM}:${PRODWEBSITE}/ 1> /dev/null
+    
+    echo "Copy to the PROD websites done"
+  fi
 exit
 EOF
--- scripts/shell/update_virtual_machines.sh
+++ scripts/shell/update_virtual_machines.sh
@@ -13,9 +13,13 @@ fi
 perldir=${LRGROOTDIR}/lrg-code/scripts/
 
 # Generate and copy the LRG progress status page
-bash ${perldir}/shell/get_lrg_step_status.sh ${output_file} 1
+#bash ${perldir}/shell/get_lrg_step_status.sh ${output_file} 1
+# DEV version
+bash ${perldir}/shell/get_lrg_step_status_test.sh ${output_file} 1
 
 # Copy the JSON LRG index
 json_file='lrg_index.json'
 json_input=${PUBFTP}/.lrg_index/${json_file}
-bash ${perldir}/shell/copy_files_to_website.sh ${json_input} js_index ${json_file}
+bash ${perldir}/shell/copy_files_to_website.sh ${json_input} json_index ${json_file}
+
+rm -f ${output_file}
