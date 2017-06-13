85fa7232ed18e236910c5e1c6eb037071392ddf1
Laurent Gil <lgil@ebi.ac.uk>
2017-06-13 14:39:24 +0100

    Add autocompletion form

--- scripts/align/transcript_alignment.php
+++ scripts/align/transcript_alignment.php
@@ -9,16 +9,22 @@
   }
   
   echo <<<EOF
-  <html>
-    <head>
-      <title>$title</title>
+<html>
+  <head>
+    <title>$title</title>
+      
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script>
+      $( function() {
+        var availableGenes = [
 
-      <!--<link type="text/css" rel="stylesheet" media="all" href="transcript_alignment.css" />
-      <script type="text/javascript" src="transcript_alignment.js"></script>-->
-
-    </head>
-    <body>
 EOF;
+
   
   $gene_id = '';
   
@@ -27,7 +33,7 @@ EOF;
   }
   
   $select_list = array();
-  
+  $first_gene = 1;
   if ($handle = opendir('./')) {
     while (false !== ($file = readdir($handle))) {
       if (preg_match('/^(\w+-?\w*)\.html$/',$file,$matches)) {
@@ -39,11 +45,32 @@ EOF;
     closedir($handle);
   }
   
+  // Generate autocomplete array for jQuery
+  $coma_flag = 0;
+  foreach ((array) $select_list as $gname) {
+    if ($coma_flag == 0) {
+      $coma_flag = 1;  
+    }
+    else {
+      echo ",\n";
+    }
+    echo '"'.$gname.'"';
+  }
+  
   echo <<<EOF
-  <div style="padding:8px 6px;background-color:#1A4468;border:1px solid #C0C0C0">
-    <div style="float:left;color:#FFF">List of available alignments:</div>
-    <div style="float:left;margin-left:10px">
-      <form style="margin-bottom:0px">
+       ];
+      $( "#autogene" ).autocomplete({
+        source: availableGenes,
+        minLength: 3
+      });
+    });
+  </script>
+  </head>
+  <body>
+    <div style="padding:8px 6px;background-color:#1A4468;border:1px solid #C0C0C0">
+      <div style="float:left;color:#FFF">List of available alignments:</div>
+      <div style="float:left;margin-left:10px">
+        <form style="margin-bottom:0px">
 EOF;
   $space = '        ';
   echo "$space<select name=\"gene\" onchange='this.form.submit()'>";
@@ -60,6 +87,17 @@ EOF;
         <noscript><input type=\"submit\" value=\"Submit\"></noscript>
       </form>
     </div>
+    <div style="float:left;;color:#FFF;margin-left:10px">
+     | or Search:
+    </div>
+    <div style="float:left;margin-left:10px">
+      <form class="form-inline" style="margin-bottom:0px">
+        <div class="form-group">
+          <input id="autogene" class="form-control ui-autocomplete-input" style="width:100px;height:22px;padding:2px 4px;" name="gene" autocomplete="off" onkeydown="javascript: if (event.keyCode==13) submit();">
+        </div>
+        <button class="btn btn-default btn-xs" type="submit">Submit</button>
+      </form>
+    </div>
     <div style="clear:both"></div>
   </div>
 EOF;
@@ -70,11 +108,13 @@ EOF;
   }
   else {
     $array_length = count($select_list);
-    echo "<h3>List of available alignments ($array_length)</h3><ul>";
+    echo "<div style=\"padding:2px 20px\">";
+    echo "  <h3>List of available alignments ($array_length)</h3>\n  <ul>";
     foreach ((array) $select_list as $gname) {
       echo "<li><a href=\"?gene=$gname\">$gname</a></li>";
     }
-    echo "</ul>";
+    echo "  </ul>";
+    echo "</div>";
   }
 
   echo <<<EOF
