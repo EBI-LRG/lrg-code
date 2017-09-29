01439888dda748e557e48b01c8c5d6a6382e5f55
Laurent Gil <lgil@ebi.ac.uk>
2017-09-29 16:08:14 +0100

    Code to generate and display the equivalent identifier tool

---
+++ scripts/equivalent_id/ens_ott_2_json.pl
@@ -0,0 +1,279 @@
+#! perl -w
+
+use strict;
+#use Getopt::Long;
+use JSON;
+
+
+my $input_file = '/homes/lgil/projets/LRG/ens_ott_table_sorted_by_HGNC_symbol.txt';
+my $output_file = '/homes/lgil/projets/LRG/ens_ott_data.json';
+my $autocomplete_file = '/homes/lgil/projets/LRG/ens_ott_autocomple.json';
+
+my $havana_file = 'hg38.bed';
+my $gencode_file = 'gencode.annotation.gtf';
+my $data_dir  = '/nfs/production/panda/production/vertebrate-genomics/lrg/data_files/';
+
+my $first_line = 1;
+my $max_json_entries_per_line = 20;
+my $max_ac_entries_per_line   = 50;
+
+my %ensts;
+my %refseq_info_list = ( 'cds_only' => 1, 'whole_transcript' => 2);
+
+# Havana update file
+my %havana_data;
+open H, "< $data_dir/$havana_file" or die $!;
+while(<H>) {
+  chomp $_;
+  my @line = split("\t", $_);
+  my $ott_id = $line[12];
+  my $hdate  = pop(@line);
+  my $date   = (split(';',$hdate))[0];
+  $havana_data{$ott_id} = $date;
+}
+close(H);
+
+
+# Gencode data file
+my %gencode_data;
+open G, "< $data_dir/$gencode_file" or die $!;
+while(<G>) {
+  chomp $_;
+  next if ($_ =~ /^#/);
+  my @line = split("\t", $_);
+  next if ($line[2] ne 'transcript');
+  
+  my @data = split(';',$line[8]);
+  my %info;
+  foreach my $data (@data) {
+    $data =~ /^\s*(\w+)\s+\"?([^\"]+)\"?/;
+    if ($1 and $2) {
+      $info{$1} = $2;
+    }
+  }
+  if (%info) {
+    my $enst = (split(/\./, $info{'transcript_id'}))[0];
+    $gencode_data{$enst} = {
+      'gname'  => $info{'gene_name'},
+      #'ensg'  => $info{'gene_id'},
+      'enst_v' => $info{'transcript_id'},
+      'tname'  => $info{'transcript_name'},
+      'ottt'   => $info{'havana_transcript'},
+      'ottg'   => $info{'havana_gene'}
+    };
+    $ensts{$enst} = 1;
+  }
+}
+close(G);
+
+
+# Ensembl data file
+my %ensembl_data;
+open E, "< $input_file" or die $!;
+my %col;
+my %autocomplete;
+
+while(<E>) {
+  chomp;
+  my @row = split("\t");
+  
+  if (!%col) {
+    for (my $i = 0; $i < scalar(@row); $i++) {
+      my $label = uc($row[$i]);
+      $col{$label} = $i;
+    }
+    next;
+  }
+  
+  my $enst_v = $row[$col{'ENST_ID'}];
+  next if ($enst_v =~ /^LRG_\d+/);
+  
+  my $enst   = (split(/\./,$enst_v))[0];
+  my $hgnc   = $row[$col{'HGNC_SYMBOL'}];
+  my $old_tr = $row[$col{'OLD_TRANSCRIPT_NAME'}];
+  my $new_tr = $row[$col{'NEW_TRANSCRIPT_NAME'}];
+  if ($hgnc !~ /\w+/) {
+    if ($old_tr && $old_tr =~ /^(\w+)-\d+$/) {
+      $hgnc = $1;
+    }
+    elsif ($new_tr && $new_tr =~ /^(\w+)-\d+$/) {
+      $hgnc = $1;
+    }
+  }
+  my $ottt   = $row[$col{'OTT_ID'}];
+  my $ccds   = $row[$col{'CCDS'}];
+  if ($ccds && $ccds =~ /^CCDS(\d+\.?\d+)$/) {
+    $ccds = $1;
+  }
+  if ($old_tr && $old_tr =~ /^$hgnc-(\d+)$/) {
+    $old_tr = $1;
+  }
+  if ($new_tr && $new_tr =~ /^$hgnc-(\d+)$/) {
+    $new_tr = $1;
+  }
+  
+  my @refseq_data = ();
+  my $refseq_info;
+  if ($row[9] and $row[9] ne '') {
+    @refseq_data = split(':',$row[9]);
+    $refseq_info = pop(@refseq_data);
+    $refseq_info = $refseq_info_list{$refseq_info} if ($refseq_info_list{$refseq_info});
+  }
+  
+  $ensembl_data{$enst} = { "hgnc"   => $hgnc,
+                           "enst_v" => $enst_v,
+                           "old_tr" => $old_tr,
+                           "new_tr" => $new_tr,
+                         };
+  if ($ottt) {
+    $ensembl_data{$enst}{"ottt"} = $ottt;
+  }  
+  if ($ccds) {
+    $ensembl_data{$enst}{"ccds"} = $ccds;
+  }
+  if (scalar(@refseq_data)!=0) {
+    $ensembl_data{$enst}{"rseq"} = \@refseq_data;
+    $ensembl_data{$enst}{"rseq_i"} = $refseq_info;
+  }
+  
+  $ensts{$enst} = 1;
+  
+  if (!$gencode_data{$enst}) {
+    print STDERR "$enst - not in Gencode\n";
+  }         
+}
+close(E);
+
+
+
+#### Generate the JSON files ####
+
+open JSON, "> $output_file" or die $!;
+print JSON "[\n";
+my $count_json_entries = 0;
+my $json_line_content = '';
+foreach my $enst (keys(%ensts)) {
+
+  my $hgnc;
+  my $new_tr_name;
+  my $ottt;
+  my $ottg;
+  my $enst_v;
+  
+  ## Basic data ##
+  if ($gencode_data{$enst}) {
+    $hgnc        = $gencode_data{$enst}{'gname'};
+    $enst_v      = $gencode_data{$enst}{'enst_v'};
+    $ottt        = $gencode_data{$enst}{'ottt'};
+    $ottg        = $gencode_data{$enst}{'ottg'};
+    $new_tr_name = $gencode_data{$enst}{'tname'};
+    if ($new_tr_name =~ /^$hgnc-(\d+)$/) {
+      $new_tr_name = $1;
+    }
+  }
+  elsif ($ensembl_data{$enst}) {
+  
+    $hgnc        = $ensembl_data{$enst}{'hgnc'};
+    $enst_v      = $ensembl_data{$enst}{'enst_v'};
+    $ottt        = $gencode_data{$enst}{'ottt'} if ($gencode_data{$enst}{'ottt'});
+    $new_tr_name = $ensembl_data{$enst}{'new_tr'};
+    if ($new_tr_name && $new_tr_name =~ /^$hgnc-(\d+)$/) {
+      $new_tr_name = $1;
+    }
+  }
+
+  my %json_data = ( "enst" => $enst_v );
+  $json_data{"hgnc"}   = $hgnc if ($hgnc && $hgnc ne '');
+  #$json_data{"new_tr"} = $new_tr_name if ($new_tr_name);
+  $json_data{"ottt"}   = $ottt if ($ottt);
+  $json_data{"ottg"}   = $ottg if ($ottg);
+  
+  # Autocomplete data
+  $autocomplete{$enst} = 1;
+  if ($hgnc && $hgnc ne '') {
+    $autocomplete{$hgnc} = 1;
+  }
+  if ($ottt) {
+    my $ottt_no_v = (split(/\./,$ottt))[0];
+    $autocomplete{$ottt_no_v} = 1;
+  }
+  if ($ottg) {
+    my $ottg_no_v = (split(/\./,$ottg))[0];
+    $autocomplete{$ottg_no_v} = 1;
+  }
+  
+  ## More specific data ##
+  
+  # Retrieve data from Ensembl hash
+  my $old_tr_name;
+  if ($ensembl_data{$enst}) {
+    $old_tr_name = $ensembl_data{$enst}{'old_tr'} if ($ensembl_data{$enst}{'old_tr'});
+    if ($ensembl_data{$enst}{'ccds'}) {
+      $json_data{"ccds"} = $ensembl_data{$enst}{'ccds'};
+    }
+    if ($ensembl_data{$enst}{'rseq'}) {
+      my $refseq_data = $ensembl_data{$enst}{'rseq'};
+      $json_data{"rseq"} = $refseq_data;
+      $json_data{"rseq_i"} = $ensembl_data{$enst}{'rseq_i'};
+      foreach my $refseq (@$refseq_data) {
+        my $refseq_no_v  = (split(/\./,$refseq))[0];
+        $autocomplete{$refseq_no_v} = 1;
+      }
+    }
+  }
+  
+  if ($old_tr_name || $new_tr_name) {
+    $old_tr_name = '' if (!$old_tr_name);
+    $new_tr_name = '' if (!$new_tr_name);
+    $json_data{"tnames"} = [$old_tr_name,$new_tr_name];
+  }
+  
+  # Retrieve data from Havana hash
+  if ($ottt) {
+    my $ottt_label = (split(/\./, $ottt))[0];
+    if ($havana_data{$ottt_label}) {
+      $json_data{"ottt_date"} = $havana_data{$ottt_label};    
+    }
+  }
+  
+  if ($count_json_entries == $max_json_entries_per_line) {
+    print JSON "$json_line_content,\n";
+    $json_line_content = '';
+    $count_json_entries = 0;
+  }
+  
+  my $json = encode_json \%json_data;
+  
+  $json_line_content .= ',' if ($json_line_content ne '');
+  $json_line_content .= $json;
+  $count_json_entries ++;
+}
+print JSON "$json_line_content" if ($json_line_content ne '');
+print JSON "\n]";
+close(JSON);
+
+
+open JSON2, "> $autocomplete_file" or die $!;
+print JSON2 "[\n";
+my $count_ac_entries = 0;
+my $ac_line_content = '';
+foreach my $id (sort(keys(%autocomplete))) {
+  next if ($id !~ /\w+/ || $id =~ /^\d+$/);
+  
+  if ($count_ac_entries == $max_ac_entries_per_line) {
+    print JSON2 "$ac_line_content,\n";
+    $ac_line_content = '';
+    $count_ac_entries = 0;
+  }
+  
+  my %json_data = ("id" => $id);
+  my $json = encode_json \%json_data;
+  $ac_line_content .= ',' if ($ac_line_content ne '');
+  $ac_line_content .= $json;
+  $count_ac_entries ++;
+}
+
+print JSON2 "$ac_line_content" if ($ac_line_content ne '');
+print JSON2 "\n]";
+close(JSON2);
+
---
+++ scripts/equivalent_id/search_results.js
@@ -0,0 +1,351 @@
+// Links
+var ens_url     = 'http://www.ensembl.org/Homo_sapiens/Location/View?t=';
+var ccds_url    = 'http://www.ncbi.nlm.nih.gov/projects/CCDS/CcdsBrowse.cgi?REQUEST=ALLFIELDS&DATA=';
+var hgnc_url    = 'http://www.genenames.org/cgi-bin/gene_symbol_report?match=';
+var hgnc_id_url = 'http://www.genenames.org/cgi-bin/gene_symbol_report?hgnc_id=';
+
+var fasta_url_prefix = 'http://rest.ensembl.org/sequence/id/';
+var fasta_url_suffix = '?content-type=text/x-fasta;type=cdna';
+
+var json_file      = './ens_ott_data.json';
+var json_file_auto = './ens_ott_autocomple.json';
+
+var external_link_class = 'icon-ext';
+
+var table_id = "#search_results";
+
+var json_keys = { 'ENST'    : 'enst',
+                  'OTTHUMT' : 'ottt',
+                  'OTTHUMG' : 'ottg',
+                  //'N(M|R)_': 'refseq'
+                };
+var json_unique_keys = { 'enst' : 1,
+                         'ottt' : 1
+                       };
+
+var rseq_i_keys = { 1 : 'cds_only', 2 : 'whole_transcript'};
+
+//
+// Methods //
+//
+
+function get_query () {
+
+  // Comes from the search page
+  var query = $('#search_id').val();
+  if (query.length > 0) {
+    wait_for_results();
+    // Asynch AJAX call + display results
+    get_search_results(query).then(function(result_objects){
+      display_results(result_objects);
+    });
+  }
+}
+
+function get_search_results (search_id) {
+
+  if (!search_id) {
+    return "No term to search";
+  }
+
+  var search_term = search_id;
+  search_term = search_term.replace(/;/g, ' / ');
+
+  $("#search_term").html(search_term);
+  $("#search_count").hide();
+
+  var result_objects = {};
+  var search_ids_list = search_id.split(';');
+  
+  return $.getJSON( json_file )
+  .error(function (xhRequest, ErrorText, thrownError) {
+        console.log('xhRequest: ' + xhRequest + "\n");
+        console.log('ErrorText: ' + ErrorText + "\n");
+        console.log('thrownError: ' + thrownError + "\n");
+   })
+  .then(function(data) {
+    $.each(search_ids_list, function (index, search_item) {
+      var key = '';
+      for (var jkey in json_keys) {
+        if (json_keys.hasOwnProperty(jkey)) {
+          var re = "^"+jkey;
+          if (search_item.match(re)) {
+            key = json_keys[jkey];
+            break;
+          }
+        }
+      }
+      result_objects = getObjects({}, data, key, search_item, result_objects);
+    });
+    return result_objects;
+  });
+
+}
+
+// Function get data in array
+function get_data_in_array () {
+  var data_array = [];
+  
+  return $.getJSON( json_file_auto )
+  .error(function (xhRequest, ErrorText, thrownError) {
+        console.log('xhRequest: ' + xhRequest + "\n");
+        console.log('ErrorText: ' + ErrorText + "\n");
+        console.log('thrownError: ' + thrownError + "\n");
+   })
+  .then(function(data) {
+    // Get the different searchable items
+    var data_list = {};
+    
+    for (var i in data) {
+      if (!data.hasOwnProperty(i)) continue;
+      data_array.push(data[i].id);
+    }
+    console.log("DATA ARRAY LENGTH: "+data_array.length);
+    return data_array;
+  });
+
+}
+
+// Function to display results
+function display_results (results) {
+
+  var result_count = Object.keys(results).length;
+  var result_term = "result";
+  if (!result_count) {
+    result_count = 0;
+  }
+  if (result_count > 1) {
+    result_term += "s";
+  }
+
+  $("#search_count").html(result_count + " " + result_term);
+  $("#search_count").show(400);
+  
+  $(table_id + " > tbody").empty();
+  
+  // Sort the results by LRG ID (using the numeric part of the LRG ID)
+  var result_keys = Object.keys(results);
+
+  result_keys = sortByKey(result_keys,'hgnc');
+
+  var link_separator = '<span>-</span>';
+
+  for (i in result_keys) {
+    var enst   = result_keys[i];
+    var symbol = results[enst].hgnc;
+    var ottg   = (results[enst].ottg) ? results[enst].ottg : '-';
+    var ottt   = (results[enst].ottt) ? results[enst].ottt : '-';
+        ottt  += (results[enst].ottt_date) ? "<div class=\"small txt_right\">"+results[enst].ottt_date+"</div>" : '';   
+    var ccds   = (results[enst].ccds) ? results[enst].ccds : '-';
+        ccds   = (ccds.match(/^[0-9]+/)) ? "CCDS"+ccds : ccds;
+    
+    var old_tr_name = '-';
+    var new_tr_name = '-';
+    if (results[enst].tnames) {
+      old_tr_name = (results[enst].tnames[0] == '') ? old_tr_name : results[enst].tnames[0];
+      old_tr_name = (old_tr_name.match(/^[0-9]+/)) ? symbol+"-"+old_tr_name : old_tr_name;
+      new_tr_name = (results[enst].tnames[1] == '') ? new_tr_name : results[enst].tnames[1];
+      new_tr_name = (new_tr_name.match(/^[0-9]+/)) ? symbol+"-"+new_tr_name : new_tr_name;
+    }
+    
+    var refseq = '-';
+    if (results[enst].rseq) {
+      var refseq_info = results[enst].rseq_i;
+          refseq_info = (rseq_i_keys[refseq_info]) ? rseq_i_keys[refseq_info] : refseq_info;
+          refseq      = "<div>"+results[enst].rseq.join("</div><div>")+ "</div><div class=\"small txt_right\">("+refseq_info+")</div>";
+    }
+
+    var ens_link   = get_ens_link(enst);
+    var fasta_link = get_fasta_link(enst);
+    var ccds_link  = get_ccds_link(ccds);
+
+    // HTML code
+    var newrow = $('<tr/>');
+    // HGNC Symbol
+    newrow.append(newCell(get_hgnc_link(symbol)));
+    // ENST ID;
+    newrow.append(newCell(ens_link+" "+fasta_link));
+    // OTTG
+    newrow.append(newCell(ottg));
+    // OTTT
+    newrow.append(newCell(ottt));
+    // CCDS
+    newrow.append(newCell(ccds_link));
+    // Old transcript name
+    newrow.append(newCell(old_tr_name));
+    // New transcript name
+    newrow.append(newCell(new_tr_name));
+    // RefSeq ID
+    newrow.append(newCell(refseq));
+    $(table_id + " > tbody").append(newrow);
+  }
+}
+
+
+function newCell(content) {
+  return $("<td></td>").html(content);
+}
+
+/**** Links ****/
+
+/* HGNC link */
+function get_hgnc_link (symbol,is_id) {
+  var ext_link = build_external_link(symbol);
+    if (is_id) {
+      ext_link.attr('onclick',"hgnc_id_link('"+symbol+"')");
+    }
+    else {
+      ext_link.attr('onclick',"hgnc_link('"+symbol+"')");
+    }
+  return ext_link[0].outerHTML;
+}
+function hgnc_link (symbol) {
+  window.open(hgnc_url+symbol,'_blank');
+}
+function hgnc_id_link (id) {
+  window.open(hgnc_id_url+id,'_blank');
+}
+
+
+/* Ensembl link */
+function get_ens_link (tr_stable_id) {
+  var ext_link = build_external_link(tr_stable_id);
+      ext_link.attr('onclick',"ensembl_link('"+tr_stable_id+"')");
+  return ext_link[0].outerHTML;
+}
+function ensembl_link (tr_stable_id) {
+  window.open(ens_url+tr_stable_id,'_blank');
+}
+
+
+/* FASTA link */
+function get_fasta_link (tr_stable_id) {
+  ext_link = $('<a></a>');
+  ext_link.addClass('fa_button label-primary');
+  ext_link.html("FA");
+  ext_link.attr('target','_blank');
+  ext_link.attr('title','FASTA sequence');
+  ext_link.attr('onclick',"fasta_link('"+tr_stable_id+"')");
+  return ext_link[0].outerHTML;
+}
+function fasta_link (tr_stable_id) {
+  window.open(fasta_url_prefix+tr_stable_id+fasta_url_suffix,'_blank');
+}
+
+/* CCDS link */
+function get_ccds_link (ccds_id) {
+  if (ccds_id == '-') {
+    return ccds_id;
+  }
+  var ext_link = build_external_link(ccds_id);
+      ext_link.attr('onclick',"ccds_link('"+ccds_id+"')");
+  return ext_link[0].outerHTML;
+}
+function ccds_link (ccds_id) {
+  window.open(ccds_url+ccds_id,'_blank');
+}
+
+
+/**** Functions to build the links ****/
+
+function build_link_base (label,url) {
+  var ext_link = $('<a></a>');
+      ext_link.html(label);
+  if (url) {
+    ext_link.attr('href', url);
+  }
+  return ext_link;
+}
+
+function build_ftp_link (label,url) {
+  var ftp_link = build_link_base(label,url);
+      
+  return ftp_link;
+}
+
+// Function to build simple external link
+function build_external_link (label,url,return_html) {
+  var ext_link = build_link_base(label,url);
+      ext_link.addClass(external_link_class);
+  if (return_html) {
+    ext_link.attr('target','_blank');
+    return ext_link[0].outerHTML;
+  }
+  else {
+    return ext_link;
+  }
+}
+
+
+// Return an array of objects according to key, value, or key and value matching
+function getObjects (obj_parent, obj, key, val, objects, regex) {
+
+  // Initialise hash
+  if (!objects) {
+    objects = {};
+  }
+  
+  if (json_unique_keys[key] && Object.keys(objects).length > 0) {
+    return objects;
+  }
+
+  // Get a search result
+  
+  // Search with regex
+  if (!regex) {
+    // Specific regex for the sequence identifiers, with a version, e.g. NM_000088.3
+    if (val.match(/^(NM_|NR_|ENST|OTTHUMG|OTTHUMT)\d+$/)) {
+      regex = new RegExp("^"+val+"\.*", "i");
+    }
+    // Wild card character associated with other characters
+    else if (val.match(/\*/)) {
+      var tmp_val = val.replace(/\*/g,".*");
+      regex = new RegExp("^"+tmp_val+"$", "i");
+    }
+    // Default regex
+    else {
+      regex = new RegExp("^"+val+"$", "i"); 
+    }
+  }
+  
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+      objects = getObjects(obj, obj[i], key, val, objects, regex);
+    }
+    // if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+    else if ((i == key && (obj[i] == val || regex.test(obj[i])))) {
+      objects[obj.enst] = obj;
+      if (json_unique_keys[i]) {
+        return objects;
+      }
+    } 
+    // only add if the object is not already in the array
+    else if ((obj[i] == val || regex.test(obj[i])) && key == ''){
+      // Data fetched from an array
+      if (Object.keys(obj)[0] == 0) {
+        objects[obj_parent.enst] = obj_parent;
+      }
+      // Data fetched from a key/value
+      else {
+        objects[obj.enst] = obj;
+      }
+    }
+  }
+  return objects;
+}
+
+function sortByKey(array, key) {
+  return array.sort(function(a, b) {
+      var x = a[key]; var y = b[key];
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+
+// Rolling image displayed while the result is 
+function wait_for_results() {
+  $("#search_header").show();
+  $(table_id).show();
+  $(table_id + " > tbody").empty();
+  $(table_id + " > tbody").append('<tr><td colspan="8"><div class="wait"></div><div class="loader"></div></td></tr>');
+}
