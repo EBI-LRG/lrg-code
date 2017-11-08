66ae2f862afad0b98ffbb34c01b12fd10582d4a6
Laurent Gil <lgil@ebi.ac.uk>
2017-11-08 14:12:30 +0000

    Add the LRG step status JSON file to the website VM

--- scripts/shell/update_virtual_machines.sh
+++ scripts/shell/update_virtual_machines.sh
@@ -22,4 +22,9 @@ json_file='lrg_index.json'
 json_input=${PUBFTP}/.lrg_index/${json_file}
 bash ${perldir}/shell/copy_files_to_website.sh ${json_input} json_index ${json_file}
 
+# Copy the LRG step JSON index
+step_json_file='step_index.json'
+step_json_input=${PUBFTP}/.lrg_index/${step_json_file}
+bash ${perldir}/shell/copy_files_to_website.sh ${step_json_input} json_index ${step_json_file}
+
 rm -f ${output_file}
