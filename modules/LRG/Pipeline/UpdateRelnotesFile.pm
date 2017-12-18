01058864db407deb2c915d14c3214108c92ceb6f
Laurent Gil <lgil@ebi.ac.uk>
2017-12-18 13:53:22 +0000

    Fix typo in the variable name

--- modules/LRG/Pipeline/UpdateRelnotesFile.pm
+++ modules/LRG/Pipeline/UpdateRelnotesFile.pm
@@ -10,7 +10,7 @@ sub run {
   
   my $run_dir  = $self->param('run_dir');
   my $assembly = $self->param('assembly');
-  my $tmp_dir  = $self->param('new_dir');
+  my $tmp_dir  = $self->param('new_xml_dir');
   my $status   = ($self->param('is_test') == 1) ? ' 1' : ' 2';
  
   `bash $run_dir/lrg-code/scripts/shell/update_relnotes_file.sh $assembly $tmp_dir/tmp$status`;
