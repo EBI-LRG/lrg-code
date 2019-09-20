2f7e7b56c8716914a54ce1bef374b61815ea377a
Laurent Gil <lgil@ebi.ac.uk>
2019-09-20 13:40:36 +0100

    Update script name

--- scripts/shell/get_collaborators_list.sh
+++ scripts/shell/get_collaborators_list.sh
@@ -9,4 +9,4 @@ dbname=${LRGDBNAME}
 pass=${LRGDBPASS}
 perldir=${LRGROOTDIR}/lrg-code/scripts/
 
-perl ${perldir}/collaborators_list.pl -host ${host} -user ${user} -port ${port} -dbname ${dbname}
+perl ${perldir}/get_collaborators_list.pl -host ${host} -user ${user} -port ${port} -dbname ${dbname}
